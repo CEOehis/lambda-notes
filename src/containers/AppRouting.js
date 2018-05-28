553af6c38af7e4ab1e9037e4c39364c57ee93561
ceoehis <ceo.ehis@outlook.com>
2018-05-28 20:33:37 +0100

    add create note page [delivers #2]

--- src/containers/AppRouting.js
+++ src/containers/AppRouting.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from 'App';
 import NavbarMenu from 'NavbarMenu';
+import CreateNoteContainer from './CreateNoteContainer';
 
 const AppRouting = () => (
   <BrowserRouter>
     <div>
       <NavbarMenu />
       <Route exact path="/" component={App} />
+      <Route path="/create" component={CreateNoteContainer} />
     </div>
   </BrowserRouter>
 );
