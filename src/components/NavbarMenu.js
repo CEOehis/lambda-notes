5742df378586133ba3a9830a020f5cde0b16a754
ceoehis <ceo.ehis@outlook.com>
2018-05-28 12:49:19 +0100

    add navbar

---
+++ src/components/NavbarMenu.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,} from 'reactstrap';
+
+class NavbarMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Lambda Notes</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">View Your Notes</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">+ Create New Note</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavbarMenu;
+
+NavbarMenu.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NoteListContainer from 'NoteListContainer';
+import NavbarMenu from 'NavbarMenu';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h2>Your Notes:</h2>
+        <NavbarMenu />
+        <h2>Your Notes</h2>
         <NoteListContainer />
       </div>
     );
