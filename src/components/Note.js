cfe8cdb7b52082d55a0e6a2c9f21e777c5797c19
ceoehis <ceo.ehis@outlook.com>
2018-06-03 04:53:54 +0100

    make cursor pointer on hover

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,7 +9,7 @@ import {
 
 const Note = ({note, ...props}) => (
   <div style={{paddingBottom: '10px'}} key={note.id} className="col-md-4">
-    <Card onClick={() => props.history.push('notes/' + note.id)} >
+    <Card style={{cursor: 'pointer'}} onClick={() => props.history.push('notes/' + note.id)} >
       <CardBody>
         <CardTitle>{note.title}</CardTitle>
         <hr />
