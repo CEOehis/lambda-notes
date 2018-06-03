9cbd11ff205f6037d0fda1717b9781aa57f37a8f
ceoehis <ceo.ehis@outlook.com>
2018-06-03 05:06:26 +0100

    update start page instructions

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import NoteListContainer from 'NoteListContainer';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function showInstruction() {
   return (
     <div className="jumbotron">
       <h1>Welcome to Lambda Notes</h1>
+      <p>An app that lets you jot down important information and snippets as you go, so you don't lose 'em.</p>
+      <p>To start <Link to="/create">Create New Note</Link></p>
     </div>
   )
 }
